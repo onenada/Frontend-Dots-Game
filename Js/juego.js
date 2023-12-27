db24d12aa6bce5d3b7a5b88c5d2e0d7f5d5ad867
Paul Hernandez <back2bytes@gmail.com>
2023-12-27 16:16:10 -0300

    Añadi los eventos de marcar al presionar,mantener y soltar el click

--- Js/juego.js
+++ Js/juego.js
@@ -2,16 +2,23 @@
 //@link https://github.com/onenada/Frontend-Dots-Game //
 // Js Para la lógica del juego //
 
-
+// Variables // 
+var inicioMarcado=false;
 // Funciones //
+
+//Devuelve un numero random entre 0 y max 
+// @param {} max numero tope 
+//source https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(max){
     return Math.floor(Math.random() * max);
 }
 
+//Obtener y rellenar los datos del usuario con los datos guardados en session storage
 function rellenarFormularioUsuario(){
     document.getElementById("nick").value=nick;
     document.getElementById("avatarImg").src=avatarImg;
 }
+//Pintado del panel de juego dependiendo de la seleccion de tamano en index.html
 function pintarPanelJuego(){
     document.getElementById('game').style.gridTemplateColumns="repeat("+tamano+", 1fr)";
     document.getElementById('game').style.gridTemplateRows="repeat("+tamano+", 1fr)";
@@ -32,7 +39,9 @@ function programarEventosJuego(){
     const items=document.getElementsByClassName('item');
     for (let item of items){
         item.addEventListener('mousedown', comenzarMarcar);
+        item.addEventListener('mouseover', continuarMarcar);
     }
+    document.addEventListener('mouseup', finMarcar);
 }
 
 
@@ -45,8 +54,28 @@ function comenzarMarcar(event){
     if(item.classList.contains('rojo')) containerItem.classList.add('rojo');
     else if(item.classList.contains('naranja')) containerItem.classList.add('naranja');
     else if(item.classList.contains('verde')) containerItem.classList.add('verde');
-    console.log("circulo presionado");
+    if(!inicioMarcado) inicioMarcado=true;
 }
+
+// Continua el marcado de los puntos mientras el mouse este encima de los puntos
+// @param {} event 
+function continuarMarcar(event){
+    if(inicioMarcado){
+        let item=event.target;
+        let containerItem=event.target.parentElement;
+        if(item.classList.contains('rojo')) containerItem.classList.add('rojo');
+        else if(item.classList.contains('naranja')) containerItem.classList.add('naranja');
+        else if(item.classList.contains('verde')) containerItem.classList.add('verde');
+    }
+}
+// Finaliza el marcado de puntos 
+function finMarcar(){
+    inicioMarcado=false;
+    }
+
+
+
+// MAIN //
 
 //Captura de datos //
 
@@ -54,7 +83,6 @@ getUserData();
 //Comprobación de datos //
 
 if (!checkUserData()) location="index.html";
-
 
 // Rellenar Formulario //
 rellenarFormularioUsuario();
